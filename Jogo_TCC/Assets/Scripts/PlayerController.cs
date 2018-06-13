570e38b5d61d82bc080580a31d7438304d212858
Asaph Teixeira e Souza <asaph.net@gmail.com>
2018-06-13 17:05:45 -0300

    movimento

--- Jogo_TCC/Assets/Scripts/PlayerController.cs
+++ Jogo_TCC/Assets/Scripts/PlayerController.cs
@@ -10,6 +10,9 @@ public class PlayerController : MonoBehaviour {
 
 	private Rigidbody2D rb;
 
+	private bool facingRight = true;
+
+
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
@@ -17,7 +20,21 @@ public class PlayerController : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () {
+		moveInput = Input.GetAxis("Horizontal");
+		rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+		if(facingRight == false && moveInput > 0){
+			Flip();
+		}else if(facingRight == true && moveInput < 0){
+			Flip();
+		}
+	}
+
+	void Flip(){
+		facingRight = !facingRight;
+		Vector3 Scaler = transform.localScale;
+		Scaler.x *= -1;
+		transform.localScale = Scaler;
 	}
 }
