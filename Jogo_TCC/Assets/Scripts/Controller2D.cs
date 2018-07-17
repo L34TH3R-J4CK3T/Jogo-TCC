34bd605340100e233c32aad8e1ccf09c20e10c72
Asaph Teixeira e Souza <asaph.net@gmail.com>
2018-07-17 16:45:16 -0300

    Character Collision PT3

--- Jogo_TCC/Assets/Scripts/Controller2D.cs
+++ Jogo_TCC/Assets/Scripts/Controller2D.cs
@@ -16,6 +16,7 @@ public class Controller2D : MonoBehaviour {
 
 	BoxCollider2D collider;
 	RaycastOrigins raycastOrigins; 
+	public CollisionInfo collisions; 
 
 	void Start(){
 		collider = GetComponent<BoxCollider2D>();
@@ -26,6 +27,8 @@ public class Controller2D : MonoBehaviour {
 
 	public void Move(Vector3 velocity){
 		UpdateRaycastOrigins();
+		collisions.Reset();
+
 		if(velocity.x != 0){
 			HorizontalCollisions(ref velocity);
 		}
@@ -51,6 +54,9 @@ public class Controller2D : MonoBehaviour {
 			{
 				velocity.x = (hit.distance - skinWidth) * directionX;
 				rayLength = hit.distance;
+
+				collisions.left = directionX == -1;
+				collisions.right = directionX == 1;
 			}
 		}
 		
@@ -71,6 +77,9 @@ public class Controller2D : MonoBehaviour {
 			{
 				velocity.y = (hit.distance - skinWidth) * directionY;
 				rayLength = hit.distance;
+
+				collisions.below = directionY == -1;
+				collisions.above = directionY == 1; 
 			}
 		}
 		
@@ -101,5 +110,16 @@ public class Controller2D : MonoBehaviour {
 	struct RaycastOrigins {
 		public Vector2 topLeft, topRight;
 		public Vector2 bottomLeft, bottomRight;
+	}
+
+	public struct CollisionInfo{
+		public bool above,below;
+		public bool left,right;
+		 
+		 public void Reset(){
+			 above = below = false;
+			 left = right = false; 
+		 }
+
 	}
 }
